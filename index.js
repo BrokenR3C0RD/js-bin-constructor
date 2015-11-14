822777ca2426102ef06ef8e5d99ef526f7105f53
ShadowCX11 <codyeksal@gmail.com>
2015-11-13 20:31:57 -0400

    Upload package.json, index,js

---
+++ index.js
@@ -0,0 +1,75 @@
+/*
+	Create a type object.
+	
+*/
+module.exports = function(object){
+	if(typeof(object)!=="object"){
+		throw new TypeError("Parameter should be an object, got "+typeof(object))
+		return false
+	}
+	var output = {}
+	for(var level in object){
+		var temp = object[level]
+		if(typeof(temp)==="object"){
+			//Run it through this function to get the types, and then set that as the output
+			output[level]=module.exports(temp)
+		} else {
+			//Just set the output as the type
+			output[level]=typeof(temp)
+			//If it's a number, figure out more about it and set the type
+			if(output[level]==="number"){
+				if(temp.toString().indexOf(".")!==-1){
+					//If it has a decimal, it's a floating point
+					output[level]="float"
+				} else if(temp < 0){
+					//If it's negative without a decimal, it's a signed integer
+					output[level]="int"
+				} else {
+					//If it's neither a floating point or an integer, we can assume it's an unsigned integer
+					output[level]="uint"
+				}
+			}
+		}
+	}
+	return output;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
